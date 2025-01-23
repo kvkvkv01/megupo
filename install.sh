c4c65e7b4b35b7d58d7b513f6833e4f4d6df6728
Kanon <125619115+kvkvkv01@users.noreply.github.com>
2025-01-23 09:43:01 -0300

    Update install.sh
    reorganized. likely final one.

--- install.sh
+++ install.sh
@@ -22,34 +22,35 @@ service postgresql start
 su - postgres -c "psql -c \"CREATE USER meguca WITH LOGIN PASSWORD 'meguca' CREATEDB\""
 su - postgres -c "createdb -T template0 -E UTF8 -O meguca meguca"
 
-# Install Go 1.13.1
+# get Go 1.13.1
 
 wget https://dl.google.com/go/go1.13.1.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.13.1.linux-amd64.tar.gz
-echo 'export PATH=$PATH:/usr/local/go/bin' >> /etc/profile
-source /etc/profile
 
-# Install Node.js 14.5.0
+# get Node.js 14.5.0
 wget https://nodejs.org/dist/v14.5.0/node-v14.5.0-linux-x64.tar.xz
-tar -xf node-v14.5.0-linux-x64.tar.xz -C /usr/local --strip-components=1
 
 # Install Rust 1.47.0
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
 rustup install 1.47.0
-rustup default 1.47.0
-
-# For some reason I tried and it didn't run the first time so... again?
-tar -C /usr/local -xzf go1.13.1.linux-amd64.tar.gz 
-echo 'export PATH=$PATH:/usr/local/go/bin' >> /etc/profile 
-source /etc/profile
 
 # Clone project
 git clone -b v6 https://github.com/bakape/shamichan.git meguca
+
+# install go
+tar -C /usr/local -xzf go1.13.1.linux-amd64.tar.gz
+echo 'export PATH=$PATH:/usr/local/go/bin' >> /etc/profile
+source /etc/profile
+
+# install node
+tar -xf node-v14.5.0-linux-x64.tar.xz -C /usr/local --strip-components=1
+
 cd meguca
 
 # Make will fail but we want to continue
 make || true
+
+cd ..
 
 # Modify thumbnailer files 
 THUMBNAILER_PATH="$(go env GOPATH)/pkg/mod/github.com/bakape/thumbnailer/v2@v2.7.1"
@@ -58,7 +59,10 @@ cp ffmpeg-fix/ffmpeg.c "${THUMBNAILER_PATH}/ffmpeg.c"
 cp ffmpeg-fix/ffmpeg.go "${THUMBNAILER_PATH}/ffmpeg.go"
 
 # Final make attempt, should work alright
+cd meguca 
+
 make
 
 # IF EVERYTHING GOES ALRIGHT THIS GOES WELL
 ./meguca
+
